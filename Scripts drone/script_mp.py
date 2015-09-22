df87d3842ea9e9a9621fc409f05395813313e61c
unknown <charly.philippe@free.fr>
2015-09-22 23:49:49 +0200

    Ajout dossier scripts finaux

---
+++ Scripts drone/script_mp.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 21:51:26 2015
+
+@author: Charles
+
+Script à exécuter dans Mission Planner
+
+Mettre le sous-script "script_test.py" dans le même dossier que celui-ci et 
+changer les variables "root" et "script_dir" pour que ça corresponde. Attention
+les accents ne sont pas bien gérés dans les adresses.
+"""
+
+import os
+from os import system
+import subprocess as sub
+import sys
+import time
+
+print('début du script d\'exécution de mission')
+
+# Variables -------------------------------------------------------------------
+
+# emplacement du code (pour donner le path à Mission Planner et en faire le répertoire de travail)
+root = 'D:/Charles/Documents/Sumo/Dassault UAV Challenge/Code/CodeUAVChallenge'
+script_dir = 'Scripts drone'
+
+# emplacement du fichier de configuration
+conf_path = 'D:/Charles/Documents/Sumo/Dassault UAV Challenge/Code/CodeUAVChallenge'
+
+# variable de debug
+drone_status = 'disarmed'
+
+# variables de mission
+mission_running = False
+
+
+# Initialisation --------------------------------------------------------------
+
+# importe la configuration
+sys.path.append(conf_path)
+import conf_drone as cf
+
+# change le path
+path = root + '/' + script_dir
+system('cd ' + path) # modidie le répertoire de travail de cmd.exe
+os.chdir(path) # modifie le répertoire de travail de MP
+
+# importe les biblis persos
+sys.path.append(cf.libpath)
+import BibliSysteme as bibsys
+#import BibliNav as nav
+import Drone as classDrone
+
+# infos sur le drone
+coords_drone = (48.7077, 2.1602)
+alt_drone = 10                           # altitude du drone en mètres
+cap_drone = 0                            # cap du drone (en degrés)
+orientation_cam = (0,0)                  # caméra à plat, devant = haut de l'image.
+
+
+# Fonctions --------------------------------------------------------------------
+
+def quit_subprocess(p, drone_status):
+  if drone_status == 'disarmed':
+    p.kill()
+    return 1
+  else:
+    print('impossible de quitter : drone pas désarmé')
+    return 0
+
+def drone_startup():
+  # attend que le drone soit prêt et arme les moteurs
+  print('startup : attend que le drone doit prêt et arme les moteurs...')
+  sys.stdout.flush()
+  time.sleep(5)
+  print('moteurs armés')
+  drone_status = 'armed'
+  print('drone_status : ' + str(drone_status))
+  print('set_mode(guided)')
+  print('prêt à commencer la mission')
+  sys.stdout.flush()
+  return 1
+  
+
+# Démarre le drone -------------------------------------------------------------
+
+#à la fin de la fonction, le drone est prêt à décoller
+drone_startup()
+
+
+# Lance la ROF -----------------------------------------------------------------
+
+# lance le sous-processus et connecte ses entrées et sorties au présent script
+p = sub.Popen('python script_rof.py', stdin = sub.PIPE, stdout=sub.PIPE)
+print('reconnaissance d\'images lancée')
+
+t0 = time.time()
+
+
+# Initialise l'automate --------------------------------------------------------
+
+drone = classDrone.Drone((coords_drone, cap_drone, alt_drone), cf.initial_target)
+
+
+# Boucle principale ------------------------------------------------------------
+while(True):
+    
+  # envoie des arguments à p
+  args_rof = bibsys.wrap_args_rof(coords_drone, alt_drone, cap_drone)
+  p.stdin.write(args_rof + "\n")   
+  p.stdin.flush()
+  
+  # lit la sortie du script de reconnaissance de formes
+  data_str = p.stdout.readline().strip()
+  sys.stdout.flush()
+  type_objet = 'none'
+
+  # teste s'il y a eu une action de l'utilisateur
+  if data_str == 'start':
+#    print('\n')
+    print('ordre utilisateur : <start>')
+    mission_running = True
+    print('je démarre la mission')
+    t0 = time.time()
+    drone.take_off_order()
+    
+  elif data_str == 'abort':
+    # lancer la procédure de "return to launch"
+#    print('\n')    
+    print('ordre utilisateur : <abort>')
+    print('j\'arrête la mission')
+    mission_running = False  # avec ça ne prend plus en compte les formes au sol
+    drone.return_to_launch()
+    
+  elif data_str == 'manuel':
+#    print('\n')
+    print('ordre utilisateur : <manuel>')
+    print('je me mets en loiter et j\'arrête la mission')
+    mission_running = False
+    print('vous avez les commandes')
+    
+  elif data_str == 'emergency':
+    # extinction sans délai des moteurs
+#    print('\n')
+    print('ordre utilisateur : <emergency>')
+    drone.shutdown()
+    print('drone au sol, fin du programme')
+    break
+  
+  # touche "q" pour quitter, quand la mission est terminée
+  elif data_str == 'quit':
+#    print('\n')
+    print('ordre utilisateur : <quit>')
+    drone_status = 'disarmed' # pour les tests en simu on force le désarmement des moteurs
+    ret = quit_subprocess(p, drone_status) # quitte le programme (si le drone est désarmé)
+    if ret:
+      print("fin du programme principal")
+      break
+  
+  
+  # si pas d'action utilisateur :
+  else:
+    # les données reçues sont alors bien celles de la reconnaissance d'images
+    rof_data = bibsys.unwrap_output_rof(data_str)
+    # si on est à une altitude suffisante et en mission, on analyse le résultat de la ROF
+    if( alt_drone > cf.min_rof_alt and mission_running ):
+      # affiche quand on a vu un objet
+      if ( rof_data[0] != 'none' ):
+        print('\n')
+        print(data_str)
+        sys.stdout.flush()
+#      print(str(time.time() - t0))
+      # envoie les infos à l'automate, déclenche la transition appropriée
+      drone.update_status(coords_drone, cap_drone, alt_drone, rof_data)
+
+  # stoppe si la mission s'éternise
+  if (time.time() - t0 > cf.max_time):
+    print('time out, je retourne à la zone de décollage')
+    break
+
+
+# Fin du script ---------------------------------------------------------------
+
+# kill le sous-script
+# attention le script n'a peut-être pas fini la boucle, il vaut mieux lui 
+# envoyer un  signal lui disant de quitter : il peut alors fermer le flux vidéo
+p.kill()
+
+print('\nscript principal exécuté')
---
+++ Scripts drone/script_rof.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 20 16:37:13 2015
+
+@author: Charles
+
+Script mettant en oeuvre la détection d'objets et consolidation infos pour la flèche.
+
+syntaxe pour open cv 3.0.0
+
+détecte les flèches et les croix
+"""
+
+
+import cv2
+import sys
+
+
+# Variables --------------------------------------------------------------------
+
+conf_path = 'D:/Charles/Documents/Sumo/Dassault UAV Challenge/Code/CodeUAVChallenge'
+
+# infos sur le drone
+coords_drone = (48.7077, 2.1602)
+alt_drone = 10                           # altitude du drone en mètres
+cap_drone = 0                            # cap du drone (en degrés)
+orientation_cam = (0,0)                  # caméra à plat, devant = haut de l'image.
+
+# Initialisation du programme --------------------------------------------------
+
+# importe le fichier de configuration des scripts
+sys.path.append(conf_path)
+import conf_drone as cf
+
+# importe les biblis persos
+sys.path.append(cf.libpath)
+import BibliTracking as track
+import BibliConsolidateTracking as constra
+import BibliSysteme as bibsys
+import Fleche as class_fleche
+import Croix as class_croix
+import Rectangle as class_rect
+
+# Parcours déjà suivi (TODO : à modifier pour prendre en compte coords initiales drone)
+coords_decollage = (48.7076, 2.1603)
+cap_decollage = 0
+coords_1ere_fleche = (48.7077, 2.1603)
+cap_1ere_fleche = 170.5
+parcours = [ (coords_decollage, cap_decollage), (coords_1ere_fleche, cap_1ere_fleche) ]
+
+# divers
+font = cv2.FONT_HERSHEY_SIMPLEX          # police utilisée à l'image
+size_factor = 0.7                        # facteur de taille de la police
+K = cv2.getStructuringElement(cv2.MORPH_RECT,(cf.t_o,cf.t_o))          # noyau pour l'opening
+bleu = (255,0,0)
+vert = (0,255,0)
+rouge = (0,0,255)
+jaune = (0,255,255)
+blanc = (255,255,255)
+
+# instanciation de la classe flèche
+patron_fleche = cv2.imread(cf.libpath + '/' + 'patron_fleche.png') # patron : vertical (orienté vers le haut, 50x50px)
+fleche = class_fleche.Fleche(patron_fleche, cf.teinte_fleche, cf.s_max_fleche, cf.r_min_fleche, cf.r_max_fleche, [], [], [], False)
+patrons_fleche = track.creePatrons(patron_fleche, cf.pas_angle_fleche, cf.n_gauss)
+liste_coords_fleche = [[],[]]
+liste_caps_fleche = [[], []]
+for i in range(cf.taille_mem-2): liste_caps_fleche.append([]); liste_coords_fleche.append([])
+
+# instanciation de la classe croix
+patron_croix = cv2.imread(cf.libpath + '/' + 'patron_croix.png')   # patron : en "+", 50x50px
+croix = class_croix.Croix(patron_croix, cf.teinte_croix, cf.s_max_croix, cf.r_min_croix, cf.r_max_croix, [], [], False)
+patrons_croix = track.creePatrons(patron_croix, cf.pas_angle_croix, cf.n_gauss)
+liste_coords_croix = [[],[]]
+for i in range(cf.taille_mem-2): liste_coords_croix.append([])
+
+# instanciation de la classe rectangle
+rectangle = class_rect.Rectangle(cf.s_min, cf.r_min, cf.r_max, [], [], False)
+liste_coords_rectangle = [[],[]]
+for i in range(cf.taille_mem-2): liste_coords_rectangle.append([])
+
+# lancement vidéo
+capture = track.initVideoFlow(cf.is_cam_embarquee)   
+
+
+# Boucle -------------------------------------------------------------------------------------------------
+
+while(True):
+
+    # récupération infos sur le drone
+    data = sys.stdin.readline().strip()
+    sys.stdin.flush()
+    coords_drone, alt_drone, cap_drone = bibsys.unwrap_args_rof(data)
+
+    # Lecture vidéo
+    frame = track.getImage(cf.is_cam_embarquee, capture)
+   
+    # Détection objets
+    fleche.detecteFleche(frame, cf.dh, cf.n_blur, K, cf.Amin, cf.Amax, patrons_fleche, cf.pas_angle_fleche, cf.seuil_certitude_fleche, cf.sat_min, cf.val_min, cf.n_gauss)
+    croix.detecteCroix(frame, cf.dh, cf.n_blur, K, cf.Amin, cf.Amax, patrons_croix, cf.pas_angle_croix, cf.seuil_certitude_croix, cf.sat_min, cf.val_min, cf.n_gauss)
+    rectangle.detecteRectangle(frame, cf.n_blur, K, cf.Amin, cf.Amax, cf.seuil_aire, cf.n_zone, cf.v_moy, cf.epsi_ratio)
+
+    # validation détection et consolidation infos
+    donnees_fleche = constra.analyseReconnaissanceFleche(fleche, liste_coords_fleche, liste_caps_fleche, cf.taille_mem, cf.seuil_sigma_pos, cf.seuil_sigma_cap, coords_drone, cap_drone, alt_drone, orientation_cam)
+    donnees_croix = constra.analyseReconnaissanceObjet(croix, liste_coords_croix, cf.taille_mem, cf.seuil_sigma_pos, coords_drone, cap_drone, alt_drone, orientation_cam)
+    donnees_rectangle = constra.analyseReconnaissanceObjet(rectangle, liste_coords_rectangle, cf.taille_mem, cf.seuil_sigma_pos, coords_drone, cap_drone, alt_drone, orientation_cam)
+    
+    # affichage validation objets
+    if donnees_fleche[0]:          # si la flèche a été validée
+        cv2.putText(frame, 'fleche valide', (20,30), font, size_factor, rouge, 2, cv2.LINE_AA)
+    if donnees_croix[0]:
+        cv2.putText(frame, 'croix valide', (20,60), font, size_factor, jaune, 2, cv2.LINE_AA)
+    if donnees_rectangle[0]:
+        cv2.putText(frame, 'rectangle valide', (20,90), font, size_factor, blanc, 2, cv2.LINE_AA)
+
+    # vérification si objets pas encore vus, choix du plus pertinent (si plusieurs valides en même temps à l'image)
+    # si nouvel objet détecté : ajout au parcours
+    parcours, donnees_objet_discrimine = constra.discrimineObjetsValides(donnees_fleche, donnees_croix, donnees_rectangle, parcours, cf.d_seuil, coords_drone)
+    
+    # "décompactage" des résultat de l'algo
+    type_objet, coords, incertitude_coords, cap, incertitude_cap = donnees_objet_discrimine
+    
+    # Affichage flux vidéo
+    cv2.imshow('frame', frame)
+    
+    # détection interactions utilisateur (codé ici car pratique avec opencv)
+    k = cv2.waitKey(10) & 0xFF
+    user_input = ''
+    # début mission
+    if k == ord('s'):
+      user_input = 'start'
+    # arrêt mission
+    if k == ord('a'):
+      user_input = 'abort'
+    # arrêt hard mission
+    if k == ord('e'):
+      user_input = 'emergency'
+    # fin du programme
+    if k == ord('q'):
+      user_input = 'quit'
+    if k == ord('m'):
+      user_input = 'manuel'
+    
+    # communication avec le script maitre
+    # si pas d'appui sur une touche : déroulement normal (objet à "none" si on n'a rien vu)
+    if user_input == '':
+      output = bibsys.wrap_output_rof(type_objet, coords, incertitude_coords, cap, incertitude_cap)
+    # sinon : on transmet le code de l'action
+    else:
+      output = user_input
+    
+    sys.stdout.write(output + "\n")
+    sys.stdout.flush()
+   
+
+
+# Fin de la boucle ----------------------------------------------------------------------------------------------
+
+
+
